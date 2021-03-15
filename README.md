604d394d8207303c08b6ae0a93f557e35f462779
lewy192 <lewis.james.hill@outlook.com>
2021-03-15 14:20:52 +1100

    HTML and CSS shell complete

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Quiz
 This project will contain a multiple choice quiz to test your programming knowledge.
+
+
+
+## Acknowledgements
+ - [Color Scheme](https://www.schemecolor.com/pac-man-game-colors.php)
+ - [Font](http://www.classicgaming.cc/classics/pac-man/fonts)
---
+++ assets/scripts/script.js
@@ -0,0 +1,19 @@
+function timer(){
+
+}
+
+var toNextQuestion = function(){
+    var question = document.querySelector("#question")
+    var button1 = document.querySelector("#button1")
+    var button2 = document.querySelector("#button2")
+    var button3 = document.querySelector("#button3")
+    var button4 = document.querySelector("#button4")
+
+}
+
+var updateScore = function(){
+
+}
+
+
+
